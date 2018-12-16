928f981f428cb6fab98dadbccae8dab0eb5d7fff
Funnbot <dillonbayless1@gmail.com>
2018-12-15 16:00:47 -0800

    Initial Commit

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (MeshFilter), typeof (MeshRenderer), typeof (MeshCollider))]
+public class Chunk : MonoBehaviour {
+    public static int chunkSize = 16;
+    public bool update = true;
+
+    MeshFilter filter;
+    MeshCollider coll;
+
+    private Block[, , ] blocks;
+
+    void Start () {
+        filter = GetComponent<MeshFilter> ();
+        coll = GetComponent<MeshCollider> ();
+
+        blocks = new Block[chunkSize, chunkSize, chunkSize];
+        for (int x = 0; x < chunkSize; x++) {
+            for (int y = 0; y < chunkSize; y++) {
+                for (int z = 0; z < chunkSize; z++) {
+                    blocks[x, y, z] = new BlockAir ();
+                }
+            }
+        }
+        blocks[3, 5, 2] = new Block ();
+        blocks[4, 5, 2] = new BlockGrass(); 
+        UpdateChunk ();
+    }
+
+    public Block GetBlock (int x, int y, int z) {
+        return blocks[x, y, z];
+    }
+
+    void UpdateChunk () {
+        var meshData = new MeshData();
+        for (int x = 0; x < chunkSize; x++) {
+            for (int y = 0; y < chunkSize; y++) {
+                for (int z = 0; z < chunkSize; z++) {
+                    meshData = blocks[x, y, z].BlockData(this, x, y, z, meshData);
+                }
+            }
+        }
+        RenderMesh(meshData);
+    }
+
+    void RenderMesh (MeshData meshData) {
+        filter.mesh.Clear();
+        filter.mesh.vertices = meshData.vertices.ToArray();
+        filter.mesh.triangles = meshData.triangles.ToArray();
+
+        filter.mesh.uv = meshData.uv.ToArray();
+        filter.mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Scripts/MeshData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshData {
+    public List<Vector3> vertices = new List<Vector3> ();
+    public List<int> triangles = new List<int> ();
+    public List<Vector2> uv = new List<Vector2> ();
+
+    public List<Vector3> colVertices = new List<Vector3> ();
+    public List<int> colTriangles = new List<int> ();
+
+    public bool useRenderDataForCol;
+
+    public MeshData () {
+
+    }
+
+    public void AddVertex (Vector3 vertex) {
+        vertices.Add (vertex);
+        if (useRenderDataForCol) {
+            colVertices.Add (vertex);
+        }
+    }
+
+    public void AddQuadTriangles () {
+        triangles.Add (vertices.Count - 4);         
+        triangles.Add (vertices.Count - 3);         
+        triangles.Add (vertices.Count - 2);         
+        triangles.Add (vertices.Count - 4);         
+        triangles.Add (vertices.Count - 2);         
+        triangles.Add (vertices.Count - 1);
+        if (useRenderDataForCol) {
+            colTriangles.Add (vertices.Count - 4);         
+            colTriangles.Add (vertices.Count - 3);         
+            colTriangles.Add (vertices.Count - 2);         
+            colTriangles.Add (vertices.Count - 4);         
+            colTriangles.Add (vertices.Count - 2);         
+            colTriangles.Add (vertices.Count - 1);
+        }
+    }
+}
